f3f2c7dfc87b3902c38aa4c222515fed54ad4068
java.46 <anish.2428cseai2323@kiet.edu>
2025-08-21 09:56:50 +0530

    java.46

---
+++ src/MyMain1.java
@@ -0,0 +1,7 @@
+ public class MyMain1 {
+    public static void main(String[] args) {
+        CollegeStudent cs = new CollegeStudent("Ghaziabad",12);
+        cs.show_roll();
+        cs.show_address();
+    }
+}
